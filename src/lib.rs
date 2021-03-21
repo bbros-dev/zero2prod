6489b7db6f7d49d6616ea3e5bdb07413b07c850d
Begley Brothers Inc <begleybrothers@gmail.com>
2021-03-21 12:12:18 +1100

    [Add][Core] Health check
    Signed-off-by: Begley Brothers Inc <begleybrothers@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 use std::net::TcpListener;
 
+// Initially returned `impl Responder`.
+// Now explicitly state the type from `actix-web`.
+// No performance difference. Just a stylistic choice.
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+// Let's start simple: we always return a 200 OK.
+async fn subscribe(_form: web::Form<SubscriberData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct SubscriberData {
+    email: String,
+    name: String,
 }
 
 #[test]
@@ -17,9 +32,14 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     // let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // // Retrieve the port assigned by the OS
     // let port = listener.local_addr().unwrap().port();
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     // No .await here
     Ok(server)
 }
